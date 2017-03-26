85f441484d04eb8afa19d068937803b8d7fd17eb
Zhu Shengqi <zetasq@gmail.com>
2017-03-26 21:18:54 +0800

    Initial Commit

---
+++ Kaleidoscrope/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Kaleidoscrope
+//
+//  Created by Zhu Shengqi on 26/03/2017.
+//  Copyright © 2017 Zhu Shengqi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
